6090cc84bd9b61059ea8fbb69f31808f4c884813
刘维 <liuwei@liuweideMBP.lan>
2019-03-19 20:38:56 +0800

    start

---
+++ src/main/java/study_plan/study_plan/StudyPlanApplication.java
@@ -0,0 +1,13 @@
+package study_plan.study_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlanApplication.class, args);
+	}
+
+}
